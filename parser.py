fc24b29ed59531e51a0b6b92152bec2df7acd7d8
Jean Paul Cornejo <jeanpaulcornejo@gmail.com>
2019-03-13 00:02:10 -0600

    Basic line parsing.

--- parser.py
+++ parser.py
@@ -1,12 +1,30 @@
 import glob
+import re
 
-# set all the files from the extracted gzip folder into a list
+# set all the files from the extracted archive into a list
 file_set = glob.glob('./smallset/*.msg')
-print(file_set)
+# print(file_set)
 
 # iterate through each file in the list
 for file_path in file_set:
-    # print(i)
+    # print(file_path)
     # open for reading
-    with open(file_path, 'r') as rf:
-        print(rf.name, end='*')
+    with open(file_path, 'r') as read_file:
+        print('File: ', read_file.name)
+
+        with open('output.txt', 'w') as write_file:
+            for line in read_file:
+                date_pattern = re.compile(r'^Date:\s*')
+                date_match = date_pattern.search(line)
+                if date_match:
+                    print(line)
+                    
+                from_pattern = re.compile(r'From:\s*')
+                from_match = from_pattern.match(line)
+                if from_match:
+                    print(line)
+
+                subject_pattern = re.compile(r'Subject:\s*')
+                subject_match = subject_pattern.match(line)
+                if subject_match:
+                    print(line)
