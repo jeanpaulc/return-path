318707db125dc7a940ddfbec554b0474f844fc3e
Jean Paul Cornejo <jeanpaulcornejo@gmail.com>
2019-03-13 17:48:39 -0600

    Var refactor

--- parser.py
+++ parser.py
@@ -25,31 +25,33 @@ def subject_search(string):
 def main():
     # iterate through each file in the list
     for file_path in file_set():
-        # open for reading; open new file for writing (append mode)
-        with open(file_path, 'r') as file_reader, open('output.txt', 'a') as file_writer:
+        # open file for reading; create new file for output
+        with open(file_path, 'r') as email_msg, open('output.txt', 'a') as output_file:
             # include file name
-            file_writer.write('\n' + file_reader.name + '\n')
+            output_file.write('\n' + email_msg.name + '\n')
 
             #iterate through each line
-            for line in file_reader:
+            for line in email_msg:
                 # read each line and use regex to match 'Sent:' header in message
                 date_match = date_search(line)
                 if date_match:
                     # remove Date key and write value to file
-                    file_writer.write(line.split('Date:')[1].strip() + '\n')
+                    date = line.split('Date:')[1].strip() + '\n'
+                    output_file.write(date)
 
                 # read each line and use regex to match 'From:' header in message
                 sender_match = sender_search(line)
                 if sender_match:
                     # find email in the line and write to file
-                    email_str = email_search(line)
-                    file_writer.write(email_str + '\n')
+                    email = email_search(line) + '\n'
+                    output_file.write(email)
 
                 # read each line and use regex to match 'Subject: ' lines
                 subject_match = subject_search(line)
                 if subject_match:
                     # remove Subject key and write value to file
-                    file_writer.write(line.split('Subject:')[1].strip() + '\n')
+                    subject = line.split('Subject:')[1].strip() + '\n'
+                    output_file.write(subject)
 
 if __name__ == "__main__":
     main()
