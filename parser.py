17fda85179207bf61a249e904bd85679bf25f932
Jean Paul Cornejo <jeanpaulcornejo@gmail.com>
2019-03-13 09:53:43 -0600

    Refined regex for Date search

--- parser.py
+++ parser.py
@@ -14,16 +14,21 @@ for file_path in file_set:
 
         with open('output.txt', 'w') as write_file:
             for line in read_file:
-                date_pattern = re.compile(r'^Date:\s*')
+                # read each line and use regex to match 'Sent: ' dates
+                date_pattern = re.compile(r'^Date:\s*(\D{3})?,?\s*(\d{1,2})\s*(\D{3})\s*(\d{4}).*')
+                # date_pattern = re.compile(r'^Date:\s*')
                 date_match = date_pattern.search(line)
                 if date_match:
                     print(line)
-                    
+
+                # read each line and use regex to match 'From: ' emails
+                # from_pattern = re.compile(r'From:\s*(?:.*)\s*<(.*@.*)>')
                 from_pattern = re.compile(r'From:\s*')
                 from_match = from_pattern.match(line)
                 if from_match:
                     print(line)
 
+                # read each line and use regex to match 'Subject: ' lines
                 subject_pattern = re.compile(r'Subject:\s*')
                 subject_match = subject_pattern.match(line)
                 if subject_match:
