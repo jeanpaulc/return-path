abb84d593c89d8a3c110ade4b0505509f4a0f5a4
Jean Paul Cornejo <jeanpaulcornejo@gmail.com>
2019-03-13 14:06:23 -0600

    Appends results to output text file.

--- parser.py
+++ parser.py
@@ -8,30 +8,33 @@ file_set = glob.glob('./smallset/*.msg')
 for file_path in file_set:
     # print(file_path)
     # open for reading
-    with open(file_path, 'r') as read_file:
-        # print('File: ', read_file.name)
+    with open(file_path, 'r') as rf:
+        with open('output.txt', 'a') as af:
+            af.write('\n' + rf.name + '\n')
 
-        with open('output.txt', 'w') as write_file:
-            for line in read_file:
+            for line in rf:
                 # read each line and use regex to match 'Sent: ' dates
                 date_pattern = re.compile(r'^Date:\s*(\D{3})?,?\s*(\d{1,2})\s*(\D{3})\s*(\d{4}).*')
-                # date_pattern = re.compile(r'^Date:\s*')
                 date_match = date_pattern.search(line)
                 if date_match:
-                    print(line)
+                    # remove Date key and write value to file
+                    af.write(line.split('Date:')[1].strip() + '\n')
 
                 # read each line and use regex to match 'From: ' emails
-                # from_pattern = re.compile(r'From:\s*(?:.*)\s*<(.*@.*)>')
                 from_pattern = re.compile(r'From:\s*')
                 from_match = from_pattern.match(line)
                 if from_match:
-                    # print(line)
+                    # find email in the line
                     email_pattern = re.compile(r'<?([\w.-]+@[\w.-]+[^>\s])')
                     email = re.search(email_pattern, line).group(1)
-                    print(email)
+                    # write email to file
+                    af.write(email + '\n')
 
                 # read each line and use regex to match 'Subject: ' lines
                 subject_pattern = re.compile(r'^Subject:\s*', re.M)
                 subject_match = subject_pattern.match(line)
                 if subject_match:
-                    print(line)
+                    # remove Subject key and write value to file
+                    af.write(line.split('Subject:')[1].strip() + '\n')
+                
+                
