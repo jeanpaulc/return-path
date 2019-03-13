aa0f187aa4eb3d448010093179067f8b52251942
Jean Paul Cornejo <jeanpaulcornejo@gmail.com>
2019-03-13 14:14:43 -0600

    Refactored as main function.

--- parser.py
+++ parser.py
@@ -1,42 +1,47 @@
 import glob
 import re
 
-# set all the files from the extracted archive into a list
-file_set = glob.glob('./smallset/*.msg')
+def main():
+    # set all the files from the extracted archive into a list
+    file_set = glob.glob('./smallset/*.msg')
 
-# iterate through each file in the list
-for file_path in file_set:
-    # open for reading
-    with open(file_path, 'r') as rf:
-        # open for writing (append mode)
-        with open('output.txt', 'a') as af:
-            # include file name
-            af.write('\n' + rf.name + '\n')
+    # iterate through each file in the list
+    for file_path in file_set:
+        # open for reading
+        with open(file_path, 'r') as rf:
+            # open for writing (append mode)
+            with open('output.txt', 'a') as af:
+                # include file name
+                af.write('\n' + rf.name + '\n')
 
-            #iterate through each line
-            for line in rf:
-                # read each line and use regex to match 'Sent: ' dates
-                date_pattern = re.compile(r'^Date:\s*(\D{3})?,?\s*(\d{1,2})\s*(\D{3})\s*(\d{4}).*')
-                date_match = date_pattern.search(line)
-                if date_match:
-                    # remove Date key and write value to file
-                    af.write(line.split('Date:')[1].strip() + '\n')
+                #iterate through each line
+                for line in rf:
+                    # read each line and use regex to match 'Sent: ' dates
+                    date_pattern = re.compile(
+                        r'^Date:\s*(\D{3})?,?\s*(\d{1,2})\s*(\D{3})\s*(\d{4}).*')
+                    date_match = date_pattern.search(line)
+                    if date_match:
+                        # remove Date key and write value to file
+                        af.write(line.split('Date:')[1].strip() + '\n')
 
-                # read each line and use regex to match 'From: ' emails
-                from_pattern = re.compile(r'From:\s*')
-                from_match = from_pattern.match(line)
-                if from_match:
-                    # find email in the line
-                    email_pattern = re.compile(r'<?([\w.-]+@[\w.-]+[^>\s])')
-                    email = re.search(email_pattern, line).group(1)
-                    # write email to file
-                    af.write(email + '\n')
+                    # read each line and use regex to match 'From: ' emails
+                    from_pattern = re.compile(r'From:\s*')
+                    from_match = from_pattern.match(line)
+                    if from_match:
+                        # find email in the line
+                        email_pattern = re.compile(r'<?([\w.-]+@[\w.-]+[^>\s])')
+                        email = re.search(email_pattern, line).group(1)
+                        # write email to file
+                        af.write(email + '\n')
 
-                # read each line and use regex to match 'Subject: ' lines
-                subject_pattern = re.compile(r'^Subject:\s*', re.M)
-                subject_match = subject_pattern.match(line)
-                if subject_match:
-                    # remove Subject key and write value to file
-                    af.write(line.split('Subject:')[1].strip() + '\n')
+                    # read each line and use regex to match 'Subject: ' lines
+                    subject_pattern = re.compile(r'^Subject:\s*', re.M)
+                    subject_match = subject_pattern.match(line)
+                    if subject_match:
+                        # remove Subject key and write value to file
+                        af.write(line.split('Subject:')[1].strip() + '\n')
+
+if __name__ == "__main__":
+    main()
                 
                 
